1d421d330004eb0f375053611698010dc8e85c59
CULEBRAS <FRAN.CULEBRAS@GMAIL.COM>
2015-07-24 18:36:44 +0200

    Changes.

--- app/src/main/java/info/culebrasgis/basketteammaker/MainActivity.java
+++ app/src/main/java/info/culebrasgis/basketteammaker/MainActivity.java
@@ -6,11 +6,32 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private static String[] playersList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playersList = new String[]{"Jugador1", "Jugador2", "Jugador3"};
+
+
+
+
+
+
+
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // TODO guardar lista jugadores de manera persistente.
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // TODO recuperar la lista de jugadores guardada de manera persistente.
+    }
 }
